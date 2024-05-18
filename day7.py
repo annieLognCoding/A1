3e211f93c93ca5b17778fbf8d39710f4fa149a1c
june <annie.chang@logncoding.com>
2024-05-18 14:59:35 +0900

    day 7 review

--- day7.py
+++ day7.py
@@ -1,3 +1,6 @@
+import random
+
+
 def reverse_each_word(sentence):
     words = sentence.split()
     reversed_words = [word[::-1] for word in words]
@@ -26,3 +29,100 @@ def contains_xyz(s):
                 count += 1
         i += 1
     return count
+
+# 1. Variables and Datatypes
+
+"""
+Variable: name of a value; 
+    space in memory that the computer can refer to
+
+Different data types in Python:
+    Primitives: int, float, String, boolean
+    Advanced: list, set, dict
+
+Integer vs. Float:
+    integer: whole number
+    float: decimal
+
+Variable Assignment
+    variable_name = value
+
+Type Casting
+    converting one data type to another
+    float_number = float(5)
+"""
+
+age = 25
+# Assign variable name fall to the value of "September"
+fall = "September"
+x = 50
+x = str(x)
+
+a = 10
+b = 20.5
+c = "Hello"
+# print(type(a), type(b), type(c))
+
+# concatenating Strings?
+x = "hi" + " hello"
+# print(x)
+
+name = "Sungbin"
+greeting = "Hello"
+s = name + " " + greeting
+# print(s)
+
+# Operators
+"""
+1. Mathematical, Logical, Comparison, Assignment
+    +-*/%//, (and or not), > < <= >= ==, = += -=
+
+2. x == y and x = y
+
+3. // --> integer division
+"""
+
+# x = 5
+# y = 3
+# print(a > b and b > c)
+# print(x ** y)
+# print((a + b + c)/3)
+
+def isEven(x):
+    return x % 2 == 0 #if x is even or odd
+
+# 1. if x is divisible by 2, we call it even
+# 2. if x % 2 == 0, then x is divisible by 2
+
+# print(isEven(5), isEven(14))
+
+def lastDigitOne(x):
+    return x % 10 == 1
+
+# x = 123
+
+# if(x % 2 == 0):
+#     print(f"{x} is even")
+# else:
+#     print(f"{x} is odd")
+
+# for i in range(10):
+#     x = int(random.random() * 6) + 1
+
+#     if(x < 3):
+#         print(f'{x} is less than 3')
+#     elif(x < 5):
+#         print(f'{x} is 3 or 4')
+#     else:
+#         print(f'{x} is greater than or equal to 5')
+
+
+def whatNumber(x):
+    if(x > 0):
+        return "positive"
+    elif(x == 0):
+        return "zero"
+    else:
+        return "negative"
+    
+print(whatNumber(23))
